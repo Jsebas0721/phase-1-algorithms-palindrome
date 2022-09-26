c488b1fb1b9a17e97995e38db9f9469f2b55bcc2
sebastian medina <sebastianlopez_21@hotmail.com>
2022-09-26 11:13:38 -0400

    commit tree 9380c375cac8aa6a471b7cd80ee493cdf7e6c168

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  let wordLength = word.length - 1; 
+   
+  for(let i = 0; i <  word.length; i++ ){
+    if(word.charAt(i)  === word.charAt(wordLength)){
+      wordLength - 1;
+    }else{
+      return false;
+    }
+
+    return true;
+  }
+
 }
 
 /* 
   Add your pseudocode here
+
+  1. Iterate through string "word"
+  2. if the first character of "word is equal to the last one",  return true and check the following char
+  3. Repeat this process until condition is false
 */
+
+
 
 /*
   Add written explanation of your solution here
@@ -13,6 +32,17 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+
+  console.log("Expecting true");
+  console.log("=>", isPalindrome("madam"))
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("Lopez"));
+
+  console.log("");
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
